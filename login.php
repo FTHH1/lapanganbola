0496d8e65f974501cc7c75561351f1d772b9d361
FTHH <fatihahfarell@gmail.com>
2025-04-29 10:01:57 +0700

    AKHIRNYA

--- login.php
+++ login.php
@@ -1,4 +1,25 @@
 <!DOCTYPE html>
+
+<?php
+session_start();
+
+    $koneksi=mysqli_connect("localhost","root","","databola");
+    if(isset($_POST["renter"])) {
+        $nama=$_POST["rnama"];
+        $password=$_POST["rpass"]; 
+    }
+
+    $sql = "SELECT * FROM dataanggota WHERE nama = '$nama' 
+            AND password= '$password' ";
+
+            $result=$koneksi->query($sql);
+
+    if ($result->num_rows > 0) {
+        $_SESSION['nama']= $nama;
+    header ("Location: daftar-akun.php");
+    }
+?>
+
 <html lang="en">
 <head>
     <meta charset="UTF-8">
@@ -9,16 +30,20 @@
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Nunito+Sans:ital,opsz,wght@0,6..12,200..1000;1,6..12,200..1000&display=swap" rel="stylesheet">
     <div class="welcome_kotak">
+
+    <form action="" method="post">
         <img src="subur.png">
         <br>
         <p class="welcome">Selamat Datang Di Pt. Subur Bola </br> Silahkan Masukan Info Anggota</p> 
+        
         <p class="nama">NAMA</p>
-        <input type="text" name="Masukan Nama Anggota" required>
+        <input type="text" name="rnama" required>
         <p class="pass">PASSWORD</p>
-        <input type="password" name="Masukan Passowrd" required>
+        <input type="password" name="rpass" required>
         <br>
-        <button type="Enter" required> Enter </button>
-        <a href="register.php"><button type="register" required> Buat akun </button> </a>
+        <button type="Enter" name="renter"  required> Enter </button>
+        <a href="register.php"><button type="button" required> Buat akun </button> </a>
+        </form>
     </div>
 </body>
 </html>
